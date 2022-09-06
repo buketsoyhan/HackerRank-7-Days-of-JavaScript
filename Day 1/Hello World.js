ae560e32a5bedab74d6cca0778ef97ea965e2218
Buket Soyhan <buketsoyhan1998@outlook.com>
2022-09-06 14:16:25 +0300

    First challenge of javascript

---
+++ Day 1/Hello World.js
@@ -0,0 +1,15 @@
+function processData(input) {
+    var value="Hello World!"
+    console.log(value)
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
